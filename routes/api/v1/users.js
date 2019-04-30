8cde00303c317d579065277471aa2915fd6b768a
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-04-30 17:41:26 -0600

    Adds to POST route for account creation.

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -1,3 +1,16 @@
 var express = require("express");
+var crypto = require("crypto");
 var router = express.Router();
 var User = require('../../../models').User;
+var apiKey = crypto.randomBytes(20).toString('hex');
+
+router.post("/", function(req, res, next) {
+  User.create({
+    email: req.body.email,
+    passwordDigest: passwordHelper,
+    api_key: apiKey
+  })
+});
+
+
+// passwordDigest helper method
