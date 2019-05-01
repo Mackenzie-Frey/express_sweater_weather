52adc683966a9ee1cc847938d0e986c7202d9abd
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-05-01 12:49:53 -0600

    Alters conditional statement. If/else block is function in Postman.

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -3,22 +3,65 @@ var crypto = require("crypto");
 var router = express.Router();
 var User = require('../../../models').User;
 var apiKey = crypto.randomBytes(20).toString('hex');
-pry = require('pryjs')
+pry = require('pryjs');
 
 router.post("/", function(req, res, next) {
   eval(pry.it);
-  User.create({
-    email: req.body.email,
-    passwordDigest: passwordHelper,
-    api_key: apiKey
-  })
-  .then(user => {
-    res.status(201).send(JSON.stringify(user.api_key));
-  })
-  .catch(error => {
-    res.status(500).send({ error });
-  });
+  if (req.body.password == req.body.password_confirmation) {
+    // res.status(201).send(JSON.stringify(apiKey));
+    User.create({
+      email: req.body.email,
+      passwordDigest: "passwordHelper",
+      api_key: apiKey
+    })
+    .then(user => {
+      res.status(201).send(JSON.stringify(user.api_key));
+    })
+    .catch(error => {
+      res.status(500).send({ error });
+    });
+
+  } else {
+      res.status(401).send(JSON.stringify('sorry'));
+    }
+
 });
 
+module.exports = router;
 
-// passwordDigest helper method
+
+
+
+// var express = require("express");
+// var crypto = require("crypto");
+// var router = express.Router();
+// var User = require('../../../models').User;
+// var apiKey = crypto.randomBytes(20).toString('hex');
+// pry = require('pryjs');
+//
+// router.post("/", function(req, res, next) {
+//   eval(pry.it);
+//   if (req.body.password == req.body.password_confirmation) {
+//     return User.create({
+//       email: req.body.email,
+//       passwordDigest: passwordHelper,
+//       api_key: apiKey
+//     })
+//     .then(user => {
+//       res.status(201).send(JSON.stringify(user.api_key));
+//     })
+//   }
+//     else {
+//       return User.create({
+//         email: req.body.email,
+//         passwordDigest: passwordHelper,
+//         api_key: apiKey
+//       })
+//     }
+//
+//   .catch(error => {
+//     res.status(500).send({ error });
+//   });
+// });
+//
+// module.exports = router;
