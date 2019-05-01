91803d366d2b33d5197aa5481188be3cb2f4bd5d
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-04-30 18:57:58 -0600

    Adds to POST route to create a user.

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -3,13 +3,21 @@ var crypto = require("crypto");
 var router = express.Router();
 var User = require('../../../models').User;
 var apiKey = crypto.randomBytes(20).toString('hex');
+pry = require('pryjs')
 
 router.post("/", function(req, res, next) {
+  eval(pry.it);
   User.create({
     email: req.body.email,
     passwordDigest: passwordHelper,
     api_key: apiKey
   })
+  .then(user => {
+    res.status(201).send(JSON.stringify(user.api_key));
+  })
+  .catch(error => {
+    res.status(500).send({ error });
+  });
 });
 
 
