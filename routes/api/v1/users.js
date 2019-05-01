f2e61ca8761001367b18be8a8c57a74870e8ce55
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-05-01 13:20:22 -0600

    User API key is returned with proper formatting in the response.

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -5,63 +5,26 @@ var User = require('../../../models').User;
 var apiKey = crypto.randomBytes(20).toString('hex');
 pry = require('pryjs');
 
-router.post("/", function(req, res, next) {
-  eval(pry.it);
+router. post("/", function(req, res, next) {
   if (req.body.password == req.body.password_confirmation) {
-    // res.status(201).send(JSON.stringify(apiKey));
     User.create({
       email: req.body.email,
-      passwordDigest: "passwordHelper",
+      passwordDigest: "passwordHelperFunction",
       api_key: apiKey
     })
     .then(user => {
-      res.status(201).send(JSON.stringify(user.api_key));
+      res.status(201).send(customizeJson(user));
     })
     .catch(error => {
       res.status(500).send({ error });
     });
-
   } else {
-      res.status(401).send(JSON.stringify('sorry'));
+      res.status(400).send(JSON.stringify('Bad Request - The password and password confirmation do not match.'));
     }
-
 });
 
+function customizeJson(user) {
+  return {"api_key": user.api_key};
+}
+
 module.exports = router;
-
-
-
-
-// var express = require("express");
-// var crypto = require("crypto");
-// var router = express.Router();
-// var User = require('../../../models').User;
-// var apiKey = crypto.randomBytes(20).toString('hex');
-// pry = require('pryjs');
-//
-// router.post("/", function(req, res, next) {
-//   eval(pry.it);
-//   if (req.body.password == req.body.password_confirmation) {
-//     return User.create({
-//       email: req.body.email,
-//       passwordDigest: passwordHelper,
-//       api_key: apiKey
-//     })
-//     .then(user => {
-//       res.status(201).send(JSON.stringify(user.api_key));
-//     })
-//   }
-//     else {
-//       return User.create({
-//         email: req.body.email,
-//         passwordDigest: passwordHelper,
-//         api_key: apiKey
-//       })
-//     }
-//
-//   .catch(error => {
-//     res.status(500).send({ error });
-//   });
-// });
-//
-// module.exports = router;
