8ac8e763ccbd9acf17cb9038dd10b20e429ab82c
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-05-02 16:43:27 -0600

    Adds location to lat and long functionality.

--- routes/api/v1/forecast.js
+++ routes/api/v1/forecast.js
@@ -8,15 +8,30 @@ pry = require('pryjs');
 
 router.get("/", function(req, res, next) {
   var address = req.query.location
-  User.findOne({ where: {api_key: req.body.api_key}})
+  // return here or set equal to a variable and return later
+  return User.findOne({ where: {api_key: req.body.api_key}})
   .then(user => {
     return fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.google_key}`)
   })
   .then(function(response) {
     return response.json();
   })
-  .then(function(myJson) {
-    res.send(JSON.stringify(myJson));
+  .then(function(coordResponse) {
+    var coordinates = coordResponse["results"][0]["geometry"]["location"]
+    eval(pry.it);
+
+
+    return fetch()
+
+
+
+
+
+
+
+
+
+    res.send(JSON.stringify(coordinates));
   })
   .catch(error => {
     res.status(401).send(JSON.stringify('Unauthorized'));
@@ -24,8 +39,6 @@ router.get("/", function(req, res, next) {
 });
 
 module.exports = router;
-
-// Do I really need the user arrow function?
 
 //  Is the catch error okay as a 401 ? That way if invalid api_key it returns 401.
 
