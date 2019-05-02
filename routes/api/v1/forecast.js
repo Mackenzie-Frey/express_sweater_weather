b14d0c5f5fe9fb8bb9ba6d859d60d5bf048ebb7d
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-05-02 15:26:56 -0600

    Adds functionality for fetching the lat and long for a location.

--- routes/api/v1/forecast.js
+++ routes/api/v1/forecast.js
@@ -1,29 +1,37 @@
 var express = require("express");
 var router = express.Router();
 var User = require('../../../models').User;
+var fetch = require("node-fetch");
+require('dotenv').config();
 pry = require('pryjs');
 
 
 router.get("/", function(req, res, next) {
-  var apiKey = req.body.api_key
-  // if (apiKey === undefined) {
-  //   res.status(401).send(JSON.stringify('Unauthorized'));
-  // }
-  var user = User.findOne({ where: {api_key: apiKey}})
+  var address = req.query.location
+  User.findOne({ where: {api_key: req.body.api_key}})
   .then(user => {
-    eval(pry.it);
-
-    // Send to geocoding api
-    req.query.location
-
+    return fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.google_key}`)
   })
-  // .catch(error => {
-  //   res.status(401).send(JSON.stringify('Unauthorized'));
-  // })
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    res.send(JSON.stringify(myJson));
+  })
+  .catch(error => {
+    res.status(401).send(JSON.stringify('Unauthorized'));
+  })
 });
 
 module.exports = router;
 
-
+// Do I really need the user arrow function?
 
 //  Is the catch error okay as a 401 ? That way if invalid api_key it returns 401.
+
+// if (apiKey === undefined) {
+//   res.status(401).send(JSON.stringify('Unauthorized'));
+// }
+
+
+// NOT CURRENTLY SETUP FOR INCORRECT API KEYS NOT WORKING
