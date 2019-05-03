0aea71bad349768ce781d7b77cf84a27a2028e04
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-05-03 11:11:17 -0600

    Adds favorites (locations) route.

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/api/v1/users');
 var sessionsRouter = require('./routes/api/v1/sessions');
 var forecastRouter = require('./routes/api/v1/forecast');
+var favoriteLocationsRouter = require('./routes/api/v1/favorite_locations');
 
 var app = express();
 
@@ -20,5 +21,6 @@ app.use('/', indexRouter);
 app.use('/api/v1/users', usersRouter);
 app.use('/api/v1/sessions', sessionsRouter);
 app.use('/api/v1/forecast', forecastRouter);
+app.use('/api/v1/favorites', favoriteLocationsRouter);
 
 module.exports = app;
