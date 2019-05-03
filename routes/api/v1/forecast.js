c52170f5fbaa4cf0eb8920218fa070bb50c93dd8
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-05-02 21:22:58 -0600

    Alters fetch calls. Weather object is being properly returned in Postman, some alterations are needed to remove the extra fields not specified in the spec.

--- routes/api/v1/forecast.js
+++ routes/api/v1/forecast.js
@@ -11,50 +11,45 @@ router.get("/", function(req, res, next) {
   // return here or set equal to a variable and return later
   return User.findOne({ where: {api_key: req.body.api_key}})
     .then(user => {
-      return fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.google_key}?exclude=minutely`)
+      return fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.google_key}`)
     })
       .then(function(response) {
-        eval(pry.it);
-        // Everything below this returns a 401. Put this line below into pry and go from there.
         return response.json();
       })
         .then(function(coordResponse) {
-          var coordinates = coordResponse["results"][0]["geometry"]["location"]
-          var key = process.env.dark_sky_key
-          var lat = coordinates["lat"]
-          var long = coordinates["long"]
+          var coordinates = coordResponse["results"][0]["geometry"]["location"];
+          var key = process.env.dark_sky_key;
+          var lat = coordinates["lat"];
+          var long = coordinates["lng"];
 
-          return fetch(`https://api.darksky.net/forecast/${key}/${lat},${long}`)
+          return fetch(`https://api.darksky.net/forecast/${key}/${lat},${long}?exclude=minutely`)
             .then(function(response) {
               return response.json();
             })
               .then(function(weatherResponse) {
-                res.send(JSON.stringify(parsedWeather(address, weather)));
+                res.send(JSON.stringify(parsedWeather(address, weatherResponse)));
               })
         })
-
-
   .catch(error => {
     res.status(401).send(JSON.stringify('Unauthorized'));
   })
 });
 
-
 function parsedWeather(address, weather) {
   return {
     "location": address,
-    "currently": "object",
-    "hourly": "object",
-    "daily": "object"
+    "currently": weather["currently"],
+    "hourly": weather["hourly"],
+    "daily": weather["daily"]
   };
 }
-
-
 
 
 module.exports = router;
 
 //  Is the catch error okay as a 401 ? That way if invalid api_key it returns 401.
+
+// Do I need more catches?
 
 // if (apiKey === undefined) {
 //   res.status(401).send(JSON.stringify('Unauthorized'));
@@ -62,3 +57,4 @@ module.exports = router;
 
 
 // NOT CURRENTLY SETUP FOR INCORRECT API KEYS NOT WORKING
+// DOES NOT PERFECTLY MATCH SPEC FOR API RESPONSE OF THE WEATHER (EXTRA THINGS ARE CURRENTLY INCLUDED)
