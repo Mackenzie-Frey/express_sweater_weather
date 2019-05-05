3beaf434de9d491daaad46c295839f3fef3457ce
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-05-05 15:12:44 -0600

    Refactors POST route to utilize ES6 syntax and aysnc await.

--- routes/api/v1/favorite_locations.js
+++ routes/api/v1/favorite_locations.js
@@ -4,32 +4,22 @@ var User = require('../../../models').User;
 var Location = require('../../../models').Location;
 var Favorite = require('../../../models').Favorite;
 
-router.post("/", function(req, res, next) {
-  var location = req.body.location;
-  return User.findOne({ where: {api_key: req.body.api_key}})
-    .then(user => {
-      if(!user) {
-        res.status(401).send(JSON.stringify('Unauthorized'))
-      } else {
-        return Location.findOrCreate({where: {name: location}})
-          .then(foundLocation => {
-            return Favorite.findOrCreate({where: {UserId: user.id, LocationId: foundLocation[0].id}})
-            .then(favorite => {
-              var message = {'message': `${location} has been added to your favorites`}
-              res.status(200).send(JSON.stringify(message))
-            })
-            .catch(error => {
-              res.status(500).send({ error })
-            })
-          })
-          .catch(error => {
-            res.status(500).send({ error })
-          })
-      }
-    })
-  .catch(error => {
+router.post("/", async (req, res, next) => {
+  try {
+    const location = req.body.location;
+    const user = await User.findOne({ where: {api_key: req.body.api_key}})
+    if(!user) {
+      res.status(401).send(JSON.stringify('Unauthorized'))
+    } else {
+      const foundLocation = await Location.findOrCreate({where: {name: location}})
+      const favorite = await Favorite.findOrCreate({where: {UserId: user.id, LocationId: foundLocation[0].id}})
+      const message = {'message': `${location} has been added to your favorites`}
+      res.status(200).send(JSON.stringify(message))
+    }
+  }
+  catch {
     res.status(500).send({ error })
-  })
-});
+  }
+})
 
 module.exports = router;
