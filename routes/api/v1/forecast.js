cc45a4bba8fee52b77b202b3c634558b8fdc1c49
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-05-05 13:48:34 -0600

    Refactors routes/api/v1/forecast.js to utilize async await and ES6 syntax.

--- routes/api/v1/forecast.js
+++ routes/api/v1/forecast.js
@@ -3,40 +3,29 @@ var router = express.Router();
 var User = require('../../../models').User;
 var fetch = require("node-fetch");
 require('dotenv').config();
-pry = require('pryjs');
 
-
-router.get("/", function(req, res, next) {
-  var address = req.query.location
-  return User.findOne({ where: {api_key: req.body.api_key}})
-  .then(user => {
+router.get("/", async (req, res, next) => {
+  try {
+    const address = req.query.location
+    const user = await User.findOne({ where: {api_key: req.body.api_key}})
     if(!user) {
         res.status(401).send(JSON.stringify('Unauthorized'));
     } else {
-        return fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.google_key}`)
-        .then(function(response) {
-          return response.json();
-        })
-          .then(function(coordResponse) {
-            var coordinates = coordResponse["results"][0]["geometry"]["location"];
-            var key = process.env.dark_sky_key;
-            var lat = coordinates["lat"];
-            var long = coordinates["lng"];
-
-            return fetch(`https://api.darksky.net/forecast/${key}/${lat},${long}?exclude=minutely`)
-              .then(function(response) {
-                return response.json();
-              })
-                .then(function(weatherResponse) {
-                  res.send(JSON.stringify(parsedWeather(address, weatherResponse)));
-                })
-          })
+      const coordResponse = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.google_key}`)
+      const geoResponse = await coordResponse.json();
+      const coordinates = geoResponse["results"][0]["geometry"]["location"];
+      const key = process.env.dark_sky_key;
+      const lat = coordinates["lat"];
+      const long = coordinates["lng"];
+      const forecastResponse = await fetch(`https://api.darksky.net/forecast/${key}/${lat},${long}?exclude=minutely`)
+      const weatherResponse = await forecastResponse.json();
+      res.send(JSON.stringify(parsedWeather(address, weatherResponse)));
     }
-  })
-  .catch(error => {
+  }
+  catch(error) {
     res.status(500).send({ error })
-  })
-});
+  }
+})
 
 function parsedWeather(address, weather) {
   return {
