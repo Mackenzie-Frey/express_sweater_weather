b4dddfeef452c7b33b8dfbba726f19808daf1800
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-05-05 10:58:50 -0600

    Completes location favoriting.

--- routes/api/v1/favorite_locations.js
+++ routes/api/v1/favorite_locations.js
@@ -1,19 +1,31 @@
 var express = require("express");
 var router = express.Router();
 var User = require('../../../models').User;
-pry = require('pryjs');
+var Location = require('../../../models').Location;
+var Favorite = require('../../../models').Favorite;
 
 router.post("/", function(req, res, next) {
   var location = req.body.location;
-  var api_key = req.body.api_key;
   return User.findOne({ where: {api_key: req.body.api_key}})
     .then(user => {
-      eval(pry.it);
-      
-      // user.favorites_cities.update(city: location)
-
-      // Resolve that promise
-      // user.favorites_cities["city"] = location
+      if(!user) {
+        res.status(401).send(JSON.stringify('Unauthorized'))
+      } else {
+        return Location.findOrCreate({where: {name: location}})
+          .then(foundLocation => {
+            return Favorite.findOrCreate({where: {UserId: user.id, LocationId: foundLocation[0].id}})
+            .then(favorite => {
+              var message = {'message': `${location} has been added to your favorites`}
+              res.status(200).send(JSON.stringify(message))
+            })
+            .catch(error => {
+              res.status(500).send({ error })
+            })
+          })
+          .catch(error => {
+            res.status(500).send({ error })
+          })
+      }
     })
   .catch(error => {
     res.status(500).send({ error })
