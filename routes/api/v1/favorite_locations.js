16dd0f4df61e6182aa0ce56a50dec14a53c03b86
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-05-05 15:50:03 -0600

    Integrates DELETE route for favorites.

--- routes/api/v1/favorite_locations.js
+++ routes/api/v1/favorite_locations.js
@@ -3,25 +3,25 @@ var router = express.Router();
 var User = require('../../../models').User;
 var Location = require('../../../models').Location;
 var Favorite = require('../../../models').Favorite;
+pry = require('pryjs');
 
 router.post("/", async (req, res, next) => {
   try {
-    const location = req.body.location;
     const user = await User.findOne({ where: {api_key: req.body.api_key}})
     if(!user) {
       res.status(401).send(JSON.stringify('Unauthorized'))
     } else {
+      const location = req.body.location;
       const foundLocation = await Location.findOrCreate({where: {name: location}})
       const favorite = await Favorite.findOrCreate({where: {UserId: user.id, LocationId: foundLocation[0].id}})
       const message = {'message': `${location} has been added to your favorites`}
       res.status(200).send(JSON.stringify(message))
     }
   }
-  catch {
+  catch(error) {
     res.status(500).send({ error })
   }
 })
-
 
 router.get("/", async (req, res, next) => {
   try {
@@ -30,11 +30,28 @@ router.get("/", async (req, res, next) => {
       res.status(401).send(JSON.stringify('Unauthorized'))
     } else {
       const favorites = await Favorite.find({where: {UserId: user.id}})
-
-      
+// Do weather stuff
+// res.send
     }
   }
-  catch {
+  catch(error) {
+    res.status(500).send({ error })
+  }
+})
+
+router.delete("/", async (req, res, next) => {
+  try {
+    const user = await User.findOne({ where: {api_key: req.body.api_key}})
+    if(!user) {
+      res.status(401).send(JSON.stringify('Unauthorized'))
+    } else {
+      const location = req.body.location;
+      const foundLocation = await Location.findOne({where: {name: location}})
+      const favorite = await Favorite.destroy({where: {UserId: user.id, LocationId: foundLocation.id}})
+      res.status(204).send()
+    }
+  }
+  catch(error) {
     res.status(500).send({ error })
   }
 })
